4c50885b616eb28234800b5dad370e77cf54c8f6
Borek Lupomesky <borek@lupomesky.cz>
2014-08-03 12:20:23 +0200

    Player pages generation
    Code for individual player pages generation.

--- nhdb-stats.pl
+++ nhdb-stats.pl
@@ -15,6 +15,8 @@ use NHdb;
 use Template;
 use utf8;
 
+$| = 1;
+
 
 #============================================================================
 #=== globals ================================================================
@@ -66,31 +68,195 @@ sub tty_message
 
 
 #============================================================================
-# Load data from SQL, store them as array of hashrefs.
+# Create list of player-variant pairs to be updated.
 #============================================================================
 
-sub sql_load
+sub update_schedule_players
 {
-  my $qry = shift;
-  my @arg = shift;
-  my @a;
-  my $cnt = 1;
+  my $cmd_force = shift;
+  my $cmd_variant = shift;
+  my $cmd_player = shift;
+  my ($sth, $r);
 
-  my $sth = $dbh->prepare($qry);
-  my $r;
-  if(scalar(@a) == 0) {
-    $r = $sth->execute();
+  #--- display information
+
+  tty_message("Getting list of player pages to update\n");
+  tty_message("  Forced processing enabled\n")
+    if $cmd_force;
+  tty_message("  Restricted to variants: %s\n", join(',', @$cmd_variant))
+    if scalar(@$cmd_variant);
+  tty_message("  Restricted to players: %s\n", join(',', @$cmd_player))
+    if scalar(@$cmd_player);
+
+  #--- get list of allowed variants
+  # this is either all statically-defined variants or a list
+  # of variants supplied through --variant cmdline option (checked
+  # against the statically-defined list).
+
+  my @variants_final;
+  my @variants_known = ('all', @{$NetHack::nh_def->{nh_variants_ord}});
+  if(scalar(@$cmd_variant)) {
+    for my $var (@$cmd_variant) {
+      if(grep { $var eq $_} @variants_known) {
+        push(@variants_final, $var);
+      }
+    }
   } else {
-    $r = $sth->execute(@arg);
+    @variants_final = @variants_known;
   }
-  if(!$r) {
-    return sprintf('Failed to query database (%s)', $sth->errstr());
+  tty_message(
+    "  Variants that will be processed: %s\n",
+    join(',', @variants_final)
+  );
+
+  #--- get list of all known player names
+
+  tty_message("  Loading list of all players");
+  my @player_list;
+  $sth = $dbh->prepare(q{SELECT name FROM games GROUP BY name});
+  $r = $sth->execute();
+  if(!$r) { die sprintf('Cannot get list of players (%s)', $sth->errstr()); }
+  while(my ($plr) = $sth->fetchrow_array()) {
+    push(@player_list, $plr);
   }
-  while(my $row = $sth->fetchrow_hashref()) {
-    $row->{n} = $cnt++;
-    push(@a, $row);
+  tty_message(", loaded %d players\n", scalar(@player_list));
+
+  #--- get list of existing (player, variant) combinations
+  #--- that have non-zero number of games in db
+
+  tty_message('  Loading list of player,variant combinations');
+  my %player_combos;
+  my $cnt_plrcombo = 0;
+  $sth = $dbh->prepare(
+    q{SELECT name, variant FROM update WHERE name <> ''}
+  );
+  $r = $sth->execute();
+  if(!$r) { die sprintf('Cannot get list of player,variant combos (%s)', $sth->errstr()); }
+  while(my ($plr, $var) = $sth->fetchrow_array()) {
+    $player_combos{$plr}{$var} = 1;
+    $cnt_plrcombo++;
   }
-  return \@a;
+  tty_message(", %d combinations exist\n", $cnt_plrcombo);
+
+  #--- forced update enabled
+
+  my @pages_forced;
+  if($cmd_force) {
+    for my $plr (@player_list) {
+
+      #--- if list of players is specified on the cmdline, then
+      #--- use it as filter here
+      if(
+        scalar(@$cmd_player) &&
+        !(grep { lc($plr) eq lc($_)} @$cmd_player)
+      ) {
+        next;
+      }
+
+      #--- create cartesian product, but restricted
+      #--- to existing player,variant combos
+      for my $var (@variants_final) {
+        if(exists $player_combos{$plr}{$var}) {
+          push(@pages_forced, [$plr, $var]);
+        }
+      }
+    }
+    tty_message("  Forcing update of %d pages\n", scalar(@pages_forced));
+    return(\@pages_forced);
+  }
+
+  #--- get list of updated players
+
+  tty_message("  Loading list of player updates");
+  my @pages_updated;
+  my $cnt = 0;
+  $sth = $dbh->prepare(q{SELECT * FROM update WHERE name <> '' AND upflag IS TRUE});
+  $r = $sth->execute();
+  if(!$r) { die sprintf('Cannot get list of player updates (%s)', $sth->errstr()); }
+  while(my ($var, $plr) = $sth->fetchrow_array()) {
+    $cnt++;
+    # skip entries with variants not in @variants_final
+    if(!grep { $var eq $_ } @variants_final) { next; };
+    # skip entries with players not in @player_list
+    if(!grep { $plr eq $_ } @player_list) { next; }
+    # skip list of players not specified in cmdline (if relevant)
+    # NOTE: the matching is case-insensitive
+    if(scalar(@$cmd_player)) {
+      if(!grep { lc($plr) eq lc($_) } @$cmd_player) { next; }
+    }
+    #
+    push(@pages_updated, [$plr, $var]);
+  }
+  tty_message(
+    ", %d updates, %d rejected\n",
+    scalar(@pages_updated),
+    $cnt - scalar(@pages_updated)
+  );
+  return(\@pages_updated);
+}
+
+
+#============================================================================
+# Create list of variants to be updated. Sources of the info about what
+# should be update are:
+#  1) --force command-line option
+#  2) --variant command-line option
+#  3) nethack_def.json statically defined variants
+#  4) update table in the database
+#============================================================================
+
+sub update_schedule_variants
+{
+  my $cmd_force = shift;
+  my $cmd_variant = shift;
+
+  #--- list of allowed variants targets; anything not in this array
+  #--- is invalid
+
+  my @variants_known = ('all', @{$NetHack::nh_def->{nh_variants_ord}});
+
+  #--- forced processing
+
+  my @candidates;
+  if($cmd_force) {
+    if(scalar(@$cmd_variant)) {
+      @candidates = @$cmd_variant;
+    } else {
+      @candidates = @variants_known;
+    }
+  } else {
+
+  #--- no forcing, using database
+
+    my $sth = $dbh->prepare(
+      q{SELECT variant FROM update WHERE name = '' AND upflag IS TRUE}
+    );
+    my $re = $sth->execute();
+    if(!$re) { 
+      die sprintf("Failed to read from update table (%s)" . $sth->errstr());
+    }
+    while(my ($a) = $sth->fetchrow_array()) {
+      if(scalar(@$cmd_variant)) {
+        if(!grep { lc($_) eq $a } @$cmd_variant) {
+          next;
+        }
+      }
+      push(@candidates, $a);
+    }
+  }
+
+  #--- validation
+
+  my @final;
+  for my $x (@candidates) {
+    if(grep {$_ eq $x} @variants_known) {
+      push(@final, $x);
+    }
+  }
+
+  #--- finish
+
+  return \@final;
 }
 
 
@@ -115,12 +281,56 @@ sub sql_load_logfiles
 #============================================================================
 #============================================================================
 
+sub row_fix
+{
+  my $row = shift;
+
+  #--- convert realtime to human-readable form
+
+  $row->{'realtime'} = format_duration($row->{'realtime'});
+
+  #--- include conducts in the ascended message
+
+  if($row->{'ascended'}) {
+    my @c = nh_conduct($row->{'conduct'});
+    $row->{'ncond'} = scalar(@c);
+    $row->{'tcond'} = join(' ', @c);
+    if(scalar(@c) == 0) {
+      $row->{death} = 'ascended with all conducts broken';
+    } else {
+      $row->{death} = sprintf(
+        qq{ascended with %d conduct%s intact (%s)},
+        scalar(@c), (scalar(@c) == 1 ? '' : 's'), $row->{'tcond'}
+      );
+    }
+  }
+
+  #--- game dump URL
+
+  if($logfiles->{$row->{'logfiles_i'}}{'dumpurl'}) {
+    $row->{'dump'} = url_substitute(
+      $logfiles->{$row->{'logfiles_i'}}{'dumpurl'},
+      $row
+    );
+  }
+
+  #--- realtime (aka duration)
+
+  if($row->{'variant'} eq 'ace' || $row->{'variant'} eq 'nh4') {
+    $row->{'realtime'} = '';
+  }
+}
+
+
+#============================================================================
+#============================================================================
+
 sub gen_page_info
 {
   my ($re, $sth);
   my %data;
 
-  tty_message("Generating generic stats\n");
+  tty_message('Generating generic stats');
 
   #--- get database data
 
@@ -141,6 +351,7 @@ sub gen_page_info
   $data{'cur_time'} = scalar(localtime());
   $tt->process('general.tt', \%data, 'general.html')
     or die $tt->error();
+  tty_message(", done\n");
 }
 
 
@@ -188,48 +399,8 @@ sub gen_page_recent
   my $cnt = 1;
   my @a;
   while(my $row = $sth->fetchrow_hashref()) {
-
-  #--- line numbers
-
+    row_fix($row);
     $row->{n} = $cnt++;
-
-  #--- convert realtime to human-readable form
-
-    $row->{'realtime'} = format_duration($row->{'realtime'});
-
-  #--- include conducts in the ascended message
-
-    if($row->{'ascended'}) {
-      my @c = nh_conduct($row->{'conduct'});
-      $row->{'ncond'} = scalar(@c);
-      $row->{'tcond'} = join(' ', @c);
-      if(scalar(@c) == 0) {
-        $row->{death} = 'ascended with all conducts broken';
-      } else {
-        $row->{death} = sprintf(
-          qq{ascended with %d conduct%s intact (%s)},
-          scalar(@c), (scalar(@c) == 1 ? '' : 's'), $row->{'tcond'}
-        );
-      }
-    }
-
-  #--- game dump URL
-
-    if($logfiles->{$row->{'logfiles_i'}}{'dumpurl'}) {
-      $row->{'dump'} = url_substitute(
-        $logfiles->{$row->{'logfiles_i'}}{'dumpurl'},
-        $row
-      );
-    }
-
-  #--- realtime (aka duration)
-
-  if($row->{'variant'} eq 'ace' || $row->{'variant'} eq 'nh4') {
-    $row->{'realtime'} = '';
-  }
-
-  #--- finish (push row into an array)
-
     push(@a, $row);
   }
   tty_message('loaded from db (%d lines)', scalar(@a));
@@ -252,7 +423,101 @@ sub gen_page_recent
 
 
 #============================================================================
+#============================================================================
+
+sub gen_page_player
+{
+  my $name     = shift;
+  my $variant  = shift;
+  my @variants = ('all');
+  my ($query, @arg, $sth, $r);
+  my %data;
+
+  #--- info
+
+  tty_message("Creating page for $name/$variant");
+
+  #--- prepare query
+
+  $query = q{SELECT * FROM v_ascended WHERE name = ?};
+  @arg = ($name);
+  if($variant ne 'all') {
+    $query .= ' AND variant  = ?';
+    push(@arg, $variant)
+  }
+  $sth = $dbh->prepare($query);
+  $r = $sth->execute(@arg);
+  if(!$r) {
+    return 'Database query failed (' . $sth->errstr() . ')';
+  }
+
+  #--- pull data
+
+  my $cnt = 1;
+  my @a;
+  while(my $row = $sth->fetchrow_hashref()) {
+    row_fix($row);
+    $row->{'n'} = $cnt++;
+    push(@a, $row);
+  }
+
+  #--- supply additional data
+
+  $data{'result'}   = \@a;
+  $data{'cur_time'} = scalar(localtime());
+  $data{'name'} = $name;
+  $data{'variant'} = $variant;
+  $data{'variants'} = [ 'all', @{$NetHack::nh_def->{nh_variants_ord}} ];
+  $data{'vardef'}   = $NetHack::nh_def->{'nh_variants_def'};
+
+  #--- determine filename
+
+  my $initial = substr($name, 0, 1);
+  my $file = sprintf("players/%s/%s.%s.html", $initial, $name, $variant);
+
+  #--- process template
+
+  $tt->process('player.tt', \%data, $file)
+    or die $tt->error();
+
+  #--- finish
+
+  tty_message(", done\n");
+  return undef;
+}
+
+
+#============================================================================
 # Display usage help.
+#
+# Semantics description, this should be moved into some other text, but for
+# now:
+#
+# Note: in this description "variant" means either one of the known
+# NetHack variants or 'all' pseudo-variant, that aggregates data from all
+# variants.
+#
+# --force makes the program ignore what is in the update table; in other
+# words, it will process even pages that are unaffected by new information;
+# when forcing processing of everything, the program takes the list of
+# known, statically defined variants (in file nethack_def.json, key
+# nh_variants_ord).
+#
+# --variant can be used multiple times and it limits processing to
+# specified variant(s) (specified by their short-code); this does not make
+# the program ignore the update table, though! if one wants to enforce
+# updating only certain variants, combine this option with --force
+#
+# --player will only generate pages for given player name; aggregate pages
+# (such as "Recent Games", "Ascended Games" etc.) will not be processed;
+# again, combine with --force to update player page even in the absence of
+# new information; combine this with --variant to limit processing to only
+# certain variant.
+#
+# --players, --noplayers enable/disable generating player pages; processing
+# players is enabled by default; explicitly disabling is very useful when
+# using --force, as this otherwise makes the program refresh all user pages
+# which is very time consuming
 #============================================================================
 
 sub help
@@ -261,6 +526,8 @@ sub help
   print "  --help         get this information text\n";
   print "  --variant=VAR  limit processing to specified variant(s)\n";
   print "  --force        force processing of everything\n";
+  print "  --player=NAME  update only given player\n";
+  print "  --noplayers    disable generating player pages\n";
   print "\n";
 }
 
@@ -288,10 +555,14 @@ tty_message(
 
 my @cmd_variant;
 my $cmd_force;
+my @cmd_player;
+my $cmd_players = 1;
 
 if(!GetOptions(
   'variant=s' => \@cmd_variant,
-  'force' => \$cmd_force
+  'force' => \$cmd_force,
+  'player=s' => \@cmd_player,
+  'players' => \$cmd_players
 )) {
   help();
   exit(1);
@@ -328,50 +599,43 @@ tty_message("Loaded list of logfiles\n");
 
 #--- read what is to be updated
 
-my @update_variants;
-my $sth = $dbh->prepare(qq{SELECT variant FROM update WHERE name = ''});
-my $re = $sth->execute();
-if(!$re) {
-  tty_message("Failed to read from update table (%s)\n", $sth->errstr());
-  die;
-} else {
-  while(my ($a) = $sth->fetchrow_array()) {
-    push(@update_variants, $a);
-  }
-}
+my $update_variants = update_schedule_variants($cmd_force, \@cmd_variant);
+tty_message(
+  "Following variants scheduled to update: %s\n",
+  join(',', @$update_variants)
+);
 
-#--- generate pages
+#--- generate aggregate pages
 
-my @variants;
-if($cmd_force) {
-  @variants = ('all');
-  push(@variants, @{$NetHack::nh_def->{nh_variants_ord}});
-} else {
-  @variants = @update_variants;
-}
-if(scalar(@update_variants)) {
-  tty_message("Following variants scheduled to update: %s\n", join(',', @update_variants));
-} else {
-  tty_message("No variants scheduled to update\n");
-}
-for my $var (@variants) {
-  if(scalar(@cmd_variant)) {
-    next if scalar(@cmd_variant) && !grep { $var eq lc($_) } @cmd_variant;
-  }
+for my $var (@$update_variants) {
   gen_page_recent('recent', $var);
   gen_page_recent('ascended', $var);
-
-#--- delete update flags
-
-  $dbh->do(qq{DELETE FROM update WHERE variant = '$var' AND name = ''});
-
+  $dbh->do(
+    q{UPDATE update SET upflag = FALSE WHERE variant = ? AND name = ''},
+    undef, $var
+  );
 }
+
+#--- generate per-player pages
+
+my $pages_update = update_schedule_players(
+  $cmd_force, \@cmd_variant, \@cmd_player
+);
+for my $pg (@$pages_update) {
+  gen_page_player(@$pg);
+  $dbh->do(
+    q{UPDATE update SET upflag = FALSE WHERE variant = ? AND name = ?},
+    undef, $pg->[1], $pg->[0]
+  );
+}
+
+#gen_page_player('Mandevil', 'all');
 
 #--- generic info page
 
-if($cmd_force || grep('all', @update_variants)) {
-  gen_page_info();
-}
+#if(grep('all', @$update_variants)) {
+#  gen_page_info();
+#}
 
 #--- disconnect from database
 
