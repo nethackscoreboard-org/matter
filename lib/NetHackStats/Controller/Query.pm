f0ecb309cae613943b9fce964800d20f258b3c90
aoei <jjadoyle@gmail.com>
2020-07-15 20:17:48 +0000

    Re-implement Ascended and Recent Games pages
    The ascension page (/ascended.*.html) and recent games (/recent.*.html) pages display
    either: n latest ascensions from all or a particular variant, or the n latest games
    ingested into the database from xlog data, either from all or a particular variant.
    Implement both pages with NetHackStats::Controller::Query::recent() and DB lookup
    subroutines in NetHackStats::Model::DB e.g. get_recent_games, get_recent_ascensions.
    Rename several templates with the extension .html.tt2 required by Mojo.
    
    Signed-of-by: Jo
    
    last commit broke front page, fixed now
    
    broke other stuff but fixed

--- lib/NetHackStats/Controller/Query.pm
+++ lib/NetHackStats/Controller/Query.pm
@@ -12,7 +12,7 @@ sub front {
     my %last_ascensions;
     my @variants = $self->app->nh->variants();
     for my $var (@variants) {
-        my $row = $self->app->nhdb->get_most_recent_asc_var($var);
+        my $row = $self->app->nhdb->get_most_recent_asc($var);
         $last_ascensions{$var} = $row;
     }
     my @variants_ordered = sort {
@@ -28,15 +28,50 @@ sub front {
     $self->stash(streaks => $self->app->nhdb->get_current_streaks());
 
     # now for recent ascensions
-    my @recent_ascs = $self->app->nhdb->get_n_recent_ascs(5);
+    my @recent_ascs = $self->app->nhdb->get_n_recent_ascs('all', 5);
     $self->stash(ascensions_recent => \@recent_ascs);    
 
     $self->render(template => 'front', handler => 'tt2');
 }
 
-## recent games
-#sub recent {
-#    my $self = shift;
-#}
+## recent games (deaths or ascensions)
+# takes a variant parameter or wildcard 'all',
+# found in stash->{var}
+sub recent {
+    my $self = shift;
+
+    my $var = $self->stash('var');
+    my $page = $self->stash('page');
+    my $lim = 100;
+
+    my @variants = 'all';
+    push @variants, $self->app->nh->variants();
+
+    # limit output unless looking specifically at ascensions for one variant
+    my @games;
+    if ($page eq 'ascended') {
+        if ($var ne 'all' && $var ne 'nh') {
+            @games = $self->app->nhdb->get_all_ascs($var);
+        } else {
+            @games = $self->app->nhdb->get_n_recent_ascs($var, $lim);
+        }
+    } else {
+        @games = $self->app->nhdb->get_n_recent_games($var, $lim);
+    }
+    # count games for scoreboard
+    my $i = 0;
+    while ($i < scalar @games) {
+        $games[$i]->{n} = $i + 1;
+        $i += 1;
+    }
+
+    $self->stash(result => \@games,
+                 cur_time => scalar(localtime()),
+                 variants => \@variants,
+                 vardef => $self->app->nh->variant_names(),
+                 variant => $var);
+
+    $self->render(template => $page, handler => 'tt2');
+}
 
 1;
---
+++ lib/NetHackStats/DB.pm
@@ -0,0 +1,25 @@
+package NetHackStats;
+use Mojo::Base 'Mojolicious';
+use NetHackStats::Model::DB;
+use NetHack::Config;
+use strict;
+use warnings;
+use feature 'state';
+use utf8;
+
+#use NetHackStats::Model::NH;
+#
+
+sub startup {
+    my $self = shift;
+    $self->helper(nhdb => sub { state $nhdb =
+            NetHackStats::Model::DB->new($self) });
+    $self->helper(nh => sub { state $nh =
+            NetHack::Config->new(config_file => "cfg/nethack_def.json")});
+    my $r = $self->routes;
+    #$r->any('/')->to('query#front')->name('front_test');
+    $r->any('/')->to('query#front');
+    #$r->any('/recent')->to('query#recent')->name('recent_test');
+}
+
+1;
