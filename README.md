6a47789e3c268e57723528bfe2ba96970359253a
aoei <jjadoyle@gmail.com>
2020-07-31 03:11:37 +0200

    Create init.sh script to automate docker build and run process
    init.sh prepares configuration files that are hidden from the git tree
    and runs the docker build process with docker-compose to generate images
    and containers necessary to run the containerised dev version of the site.
    It assumes a clean git tree and no pre-existing nhs docker containers.
    
    Add an additional script, reloader.sh which can rebuild and reload various
    components of an already built or partially built set of docker images
    and containers for running NHS. This should rarely if ever be necessary,
    in most cases restarting the nhs-web-m container should be sufficient.
    It should also be possible to test changes to the front-end code without
    having to reload the nhs-web-m container (running morbo) at all.
    
    Signed-off-by: Joanna/aoei
    
    Make more comprehensive init script for building and running containers
    
    Added a lot of bells and whistles to the init script.

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
+---------OVERVIEW---------
+The legacy backend code resides in legacy/ and a docker system has
+been designed so that nhdb-feeder.pl may run in its own container and
+populate the database. The postgresql database also runs in a docker
+container, and the database initialisation files are in postgres/.
+The mojo web-framework front-end code is in mojo/, which again has
+its own docker container. This is a work-in-progress and currently
+the front-page, recent, ascended, streaks, zscore pages are functional.
+In addition, /players/$name pages are available, including overview
+and per-player versions of the recent, ascended, streaks and gametime
+are available at /players/$name/recent, /../ascended, etc.
+
+----BUILD AND RUN-----
+Build and start docker services with init.sh
+The script assumes a clean git tree and no pre-existing docker
+containers or images running NHS. Passwords for nhdbfeeder/nhdbstats
+or the database password itself may be given by command-line.
+ `$ ./init.sh -f feedpw -s statspw -d dbpw`
+Additionally, if you use SELinux, you probably need to use the -S flag
+to fix SELinux flags on the directories legacy/ and mojo/ for their
+bind mounts to the built containers.
+
+An earlier version of init.sh was renamed to reloader.sh. It provides
+a lot of options for reloading and refreshing docker services to varying
+extents. In most cases it should not be needed. After the initial run
+of ./init.sh, simply running `docker compose up mojo-web` should be
+sufficient.
+
+NB: if you already have postgresql running on your docker host or another
+container with the default port 5432, the database container will fail,
+as will everything else.
+
+-----------------------
+
+However, if in some case it is desired, you can use ./reload.sh -R to
+refresh persistent volumes (e.g. the volume used by the database container).
+
+The -r flag will re-generate configuration files for postgres,
+feeder and mojo. -C will run git clean -fx, which amounts to
+essentially the same thing. These commands may change the db
+passwords, so renewal of the database volume will also be carried out.
+
+The -I flag will remove all docker images associated with the project.
+
+Note the logic for some of these volume and image removal steps is not
+very sophisticated, it is possible you will have stale containers or
+images that the init.sh script won't remove (this may be improved upon).
+
+It is also important to note that currently, changing db passwords will
+require a full reset of the DB, because of the simple way in which the
+passwords for the database are set up on initialisation. This could also
+be improved perhaps.
+
+--------- this is all taken care of by init.sh now ----------
 I suggest building the individual components rather than
 running docker-compose up. Especially if the database service
 is not yet ready.
@@ -15,7 +69,9 @@ chcon -Rt svirt_sandbox_file_t mojo cfg
         `$ chcon -Rt svirt_sandbox_file_t mojo`
         `$ chcon -Rt svirt_sandbox_file_t legacy`
     1. Initialise config/environment files for the database.
-        `$ ./cfg-init-postgres.sh -f feed_pw -s stats_pw -d db_pw`
+       Remember, changes to DB config won't update unless you remove
+       the persistent volume.
+        `$ ./postgres/init.sh -f feed_pw -s stats_pw -d db_pw`
     2. Build and run the database image/container.
         `$ docker-compose build database && docker-compose up -d database`
     3. Build the perl images.
@@ -23,9 +79,9 @@ chcon -Rt svirt_sandbox_file_t mojo cfg
         `$ docker-compose build libs-legacy`
         `$ docker-compose build libs-mojo`
     4. Run config for feeder, start container, then mojo-web
-        `$ ./cfg-init-feeder.pl`
+        `$ ./legacy/init.sh`
         `$ docker-compose build feeder`
         `$ docker-compose up feeder`
-        `$ ./cfg-init-mojo.pl`
+        `$ ./mojo/init.sh`
         `$ docker-compose build mojo-web`
         `$ docker-compose up mojo-web`
---
+++ reloader.sh
@@ -0,0 +1,159 @@
+#!/usr/bin/env bash
+helpFunction()
+{
+    echo ""
+    echo "Usage: $0 [ -CIRSbr ]"
+    echo -e "\t-C Run git clean -fx to clear .env and old config files containing db pass info etc."
+    echo -e "\t-I Rebuild all images."
+    echo -e "\t-R Refresh persistent volumes. Necessary if db passwords change."
+    echo -e "\t-S Fix SELinux tags on legacy/ and mojo/ bind-mounts (need sudo)."
+    echo -e "\t-b Rebuild image on docker-compose up commands."
+	echo -e "\t-r Refresh config files. This is default if the relevant files aren't found anyway."
+    exit 1
+}
+
+while getopts "CIRSbd:f:rs:" opt
+do
+    case $opt in
+		C ) gitclean="y" ;;		    # skip git clean - some may want this default
+        I ) reload_img="y" ;;       # rebuild all docker images for project
+        R ) refresh_vol="y" ;;      # refresh volumes for postgres etc.
+		S ) selinux="y" ;; 		    # fix SELinux tags for bind mounts - sudo req.
+		b ) build="y" ;;	        # force the build step on docker-compose up
+        d ) database_pw="$OPTARG" ;;# set database password
+        f ) feeder_pw="$OPTARG" ;;  # set feeder password
+		r ) refresh_cfg="y" ;;	    # renew config files - running init scripts
+								    # is anyway default if the configs are missing
+        s ) stats_pw="$OPTARG" ;;   # set stats password
+        ? ) helpFunction ;;
+    esac
+done
+
+# set cli args for docker-compose based on init.sh flags
+if [ "$build" == "y" ]; then
+	dc_args="--build"
+fi
+if [ "$refresh_vol" == "y" ]; then
+	dc_args="$dc_args -V"
+fi
+
+# allow setting of passwords -- requires db/cfg reload
+if [ ! -z "$feeder_pw" ]; then
+    refresh_cfg="y"
+else
+    feeder_pw=feeder
+fi
+if [ ! -z "$stats_pw" ]; then
+    refresh_cfg="y"
+else
+    stats_pw=stats
+fi
+if [ ! -z "$database_pw" ]; then
+    refresh_cfg="y"
+else
+    database_pw=data
+fi 
+
+# dunno if this is really needed, user can always call it themself
+if [ "$gitclean" == "y" ]; then
+	git clean -fx
+fi
+
+# remove running web or feeder containers, if extant
+containers=`docker container ls -a --format='{{json .Names}}' | sed s/\"//g | grep -E 'nhs-web-m|nhdb-feeder'`
+if [ ! -z "$containers" ]; then
+    echo "stopping containers $containers"
+	docker rm -f $containers
+fi
+
+# if postgres is running and neither refresh_cfg or refresh_vol are set
+# we can safely do nothing here, otherwise, kill nhs-db if it's running
+# and start a new one
+docker container ls | grep nhs-db >/dev/null
+postgres_return=$?
+if [ $postgres_return -eq 0 ] && [ "$refresh_cfg" != "y" ] && [ "$refresh_vol" != "y" ] && [ "$reload_img" != "y" ] && [ -e ./postgres/env ]; then 
+	echo "Skipping postgres restart, nhs-db still running."
+else
+	# postgres container still running - need to kill
+	if [ $postgres_return -eq 0 ]; then
+        echo "Stopping postgres container..."
+		docker rm -f nhs-db
+	fi
+	if ! [ -e ./postgres/env ] || [ "$refresh_cfg" == "y" ]; then
+        echo "Initialising Postgres config..."
+		./postgres/init.sh -f $feeder_pw -s $stats_pw -d $database_pw
+	fi
+
+    echo "Removing db persistent volume..."
+    docker volume rm -f nhs-fork_db_vol
+
+    # reload images essentially implies we're going to remove persistent volumes
+    if [ "$refresh_vol" == "y" ] || [ "$reload_img" == "y" ]; then
+        echo "Removing xlog persistent volume..."
+        docker volume rm -f nhs-fork_xlogs_vol
+    fi
+
+    # Remove images if -I flag set
+    if [ "$reload_img" == "y" ]; then
+        echo "Removing all docker images..."
+        docker image rm -f nhs-fork_{mojo-web,database,feeder} cpan-{mojo,moo} perl-base
+    fi
+
+    echo "Starting postgres container..."
+	docker-compose up $dc_args -d database
+
+	# if we had to start a new database container, IP could change,
+	# so config for other containers has to be refreshed anyway
+    # passwords may have also changed
+	refresh_cfg="y"
+
+    # Sleep for a bit or postgres won't be ready for feeder
+    sleep 30
+fi
+
+# update config for legacy feeder, should not actually mean full
+# rebuild of images/containers is necessary, since config comes
+# in through a bind mount
+if [ ! -e ./legacy/cfg/auth.json ] || [ "$refresh_cfg" == "y" ]; then
+    echo "Initialising feeder config..."
+	./legacy/init.sh
+fi
+
+if [ "$selinux" == "y" ]; then
+    echo "Attempting to fix SELinux flags"
+	sudo chcon -Rt svirt_sandbox_file_t legacy
+	sudo chcon -Rt svirt_sandbox_file_t mojo
+fi
+
+# check if we have the perl-base repository, build it otherwise
+docker image ls | grep perl-base >/dev/null
+if [ $? -ne 0 ]; then
+    docker-compose build base
+fi
+# same for the cpan modules
+docker image ls | grep cpan-moo >/dev/null
+if [ $? -ne 0 ]; then
+    docker-compose build libs-legacy
+fi
+
+# run the database feeder, attached, and wait for reading of
+# some xlogs
+echo "Start feeder container..."
+docker-compose up $dc_args feeder
+
+# update config for legacy feeder, should not actually mean full
+# rebuild of images/containers is necessary, since config comes
+# in through a bind mount
+if [ ! -e ./mojo/cfg/nethackstats.json ] || [ "$refresh_cfg" == "y" ]; then
+    echo "Initialise mojo-web config..."
+	./mojo/init.sh
+fi
+
+# check for the cpan-mojo image
+docker image ls | grep cpan-mojo >/dev/null
+if [ $? -ne 0 ]; then
+    docker-compose build libs-mojo
+fi
+
+echo "Start mojo-web container"
+docker-compose up $dc_args mojo-web
