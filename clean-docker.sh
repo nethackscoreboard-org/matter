611f076e332a4476571b82b01382e6ddeb0f319b
aoei <jjadoyle@gmail.com>
2020-07-31 03:11:37 +0200

    Create init.sh script to automate docker build and run process
    init.sh prepares configuration files that are hidden from the git tree
    and runs the docker build process with docker-compose to generate images
    and containers necessary to run the containerised dev version of the site.
    It assumes a clean git tree and no pre-existing nhs docker containers.
    
    Add an additional script, reloader.sh which can rebuild and reload various
    components of an already built or partially built set of docker images
    and containers for running NHS. This should rarely if ever be necessary,
    in most cases restarting the nhs-web-m container should be sufficient.
    It should also be possible to test changes to the front-end code without
    having to reload the nhs-web-m container (running morbo) at all.
    
    Signed-off-by: Joanna/aoei
    
    Make more comprehensive init script for building and running containers
    
    Added a lot of bells and whistles to the init script.

--- clean-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-# this should not be something done regularly during development
-# however, I want to test that the build system reliably produces
-# something out of the box given a fresh git repo and no docker
-# images/containers
-docker rm -f `docker container ls -aq`
-docker image rm `docker image ls -aq`
-docker volume rm `docker volume ls -q`
