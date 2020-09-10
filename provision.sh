6f08ff70037bfa9d1c24bbf5567384300f36ae4c
Joanna Doyle <jjadoyle@gmail.com>
2020-09-10 19:01:57 +0200

    fiddle with scripts, test again - main problem still gpg --search-keys/--receive-keys

--- provision.sh
+++ provision.sh
@@ -4,13 +4,12 @@
 #                        --security-opt label=disable        \
 #                        --volume ${PWD}:/pwd --workdir /pwd \
 #                        quay.io/coreos/coreos-installer:release'
-set -x
-
 export yaml=docker-cg1.yml
 export vm_name=kizul
 export config=${vm_name}.json
 export stream=next
 export serial=yes
+export domain=gcloud.ext
 
 alias ignition-validate='podman run --rm --tty --interactive \
                          --security-opt label=disable        \
@@ -20,6 +19,13 @@ alias ignition-validate='podman run --rm --tty --interactive \
 alias fcct='podman run --rm --tty --interactive --security-opt \
             label=disable --volume ${PWD}:/pwd --workdir /pwd \
             quay.io/coreos/fcct:release'
+
+# --kill
+if [[ $# -eq 1 ]] && [[ "$1" == "--kill" ]]; then
+    gcloud compute instances delete $vm_name
+    sed -iE "s/^${vm_name}.\*//" ~/.ssh/known_hosts
+    sed -E "s/^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\.)[[:space:]]+${vm_name}\.${domain}[[:space]]+${vm_name}\$/old ip was \1/" /etc/hosts # we don't need the old ip why am i doing this
+fi
 
 
 fcct --pretty --strict $yaml --output $config
