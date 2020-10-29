f583f85cf002eced2963a8e990b151e631fb931d
Joanna Doyle <jjadoyle@gmail.com>
2020-10-29 02:07:10 +0100

    Fix: assign IP and host so web/feed can access db
    Add explicit subnet and IP configurations to docker-cg1.yml, as docker
    does not automatically add host info to /etc/hosts in a container, for
    containers on the same network.
    
    Switch provision.sh to using system fcct instead of podman alias -
    podman currently not working on my local machine. Add to provision.sh a
    --zone flag for situations where the default zone is full.
    
    Modify relative paths in www/templates/player.html.tt2 and
    www/templates/player/topmenu.html.tt2 to try and fix the tabsort JS
    issue reported by Luxidream.
    
    Signed-off-by: Joanna Doyle <jjadoyle@gmail.com>

--- provision.sh
+++ provision.sh
@@ -16,28 +16,45 @@ alias ignition-validate='podman run --rm --tty --interactive \
                          --volume ${PWD}:/pwd --workdir /pwd \
                          quay.io/coreos/ignition-validate:release'
 
-alias fcct='podman run --rm --tty --interactive --security-opt \
-            label=disable --volume ${PWD}:/pwd --workdir /pwd \
-            quay.io/coreos/fcct:release'
+#alias fcct='podman run --rm --tty --interactive --security-opt \
+#            label=disable --volume ${PWD}:/pwd --workdir /pwd \
+#            quay.io/coreos/fcct:release'
 
 # --kill
-if [[ $# -eq 1 ]] && [[ "$1" == "--kill" ]]; then
-    gcloud compute instances delete $vm_name
-    grep -v "^$vm_name" ~/.ssh/known_hosts >~/.ssh/known_hosts-
-    grep -v "$vm_name" /etc/hosts >~/.ssh/hosts
+while [[ $# -gt 0 ]]; do
+    if [[ $# -ge 1 ]] && [[ "$1" == "--kill" ]]; then
+        kill_flag="true"
+        shift
+    elif [[ $# -ge 2 ]] && [[ "$1" == "--zone" ]]; then
+        zonearg="--zone=$2"
+        shift; shift
+    fi
+done
+
+if [[ -n "$kill_flag" ]]; then
+    gcloud compute instances delete $vm_name $zonearg
 fi
 
+grep -v "^$vm_name" ~/.ssh/known_hosts >~/.ssh/known_hosts-
+grep -v "$vm_name" /etc/hosts >~/.ssh/hosts
 
 fcct --pretty --strict $yaml --output $config
 
 # https://unix.stackexchange.com/questions/150957/generating-file-with-ascii-numbers-using-dev-urandom
-ip=$(gcloud compute instances create --metadata-from-file "user-data=${config:-}" --image-project "fedora-coreos-cloud" --image-family "fedora-coreos-${stream:-}" "${vm_name:-}" | grep -E "^$vm_name" | sed -E 's/[[:space:]]+/ /g' | cut -d' ' -f5)
+output=$(gcloud compute instances create ${zonearg} --metadata-from-file "user-data=${config:-}" --image-project "fedora-coreos-cloud" --image-family "fedora-coreos-${stream:-}" "${vm_name:-}")
+
+# crash out if it failed
+if [[ $? -ne 0 ]]; then
+    exit $?
+fi
+
+ip=$(echo $output | grep -E "^$vm_name" | sed -E 's/[[:space:]]+/ /g' | cut -d' ' -f5)
 
 # update stuff
 vm_full=${vm_name}.${domain}
-echo $ip    $vm_full    $vm_name | tee -a ~/.ssh/hosts
+echo $ip    $vm_full    $vm_name >> ~/.ssh/hosts
 sudo cp ~/.ssh/hosts /etc/hosts
 mv ~/.ssh/known_hosts- ~/.ssh/known_hosts
 
 
-[ -n "${serial:-}" ] && gcloud compute instances add-metadata ${vm_name:-} --metadata serial-port-enable=TRUE
+[ -n "${serial:-}" ] && gcloud compute instances add-metadata ${vm_name:-} --metadata serial-port-enable=TRUE ${zonearg}
