e4f8f1d3a88c935f6351c5e18699db40abde958d
Joanna Doyle <jjadoyle@gmail.com>
2021-01-14 23:06:06 +0100

    Filter explore/wizmode games, fix required fields
    old NAO games were filtered on import because of the requirement
    that the xlog entry contain "conduct", "starttime" and "endtime" fields.
    until 2008, there was no conduct field, and only startdate and enddate.
    
    this commit also adds filtering of xlog entries if they contain the flag
    0x1 or 0x2 (wizard and explore modes, respectively)
    
    Signed-off-by: Joanna Doyle <jjadoyle@gmail.com>

--- nhdb-feeder.pl
+++ nhdb-feeder.pl
@@ -525,13 +525,12 @@ sub sql_insert_games
       $xlog_data->{'flags'} = hex $xlog_data->{'flags'};
     }
 
-#    # 0x1 is the flag for WIZARD MODE, 0x2 is EXPLORE (vanilla nethack)
-#    # 0x4 is the polylinit flag in xNetHack
-#    if(($variant eq 'nh' && ($xlog_data->{'flags'} & 0x1 || $xlog_data->{'flags'} & 0x2))
-#      || ($variant eq 'xnh' && ($xlog_data->{'flags'} & 0x4 || $xlog_data->{'flags'} & 0x1 || $xlog_data->{'flags'} & 0x2)))
-#    {
-#      return undef;
-#    }
+    # 0x1 is the flag for WIZARD MODE, 0x2 is EXPLORE (vanilla nethack)
+    # 0x4 is the polylinit flag in xNetHack
+    if($xlog_data->{'flags'} & 0x1 || $xlog_data->{'flags'} & 0x2)
+    {
+      return undef;
+    }
   }
 
   #--- reject "special" modes of NH4 and its kin
